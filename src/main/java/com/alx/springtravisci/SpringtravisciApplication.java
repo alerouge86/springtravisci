33986b5c417eed406a7684884ea58a2ed52ec95c
Alessandro Rossi <arossi@siapcn.it>
2021-06-11 08:41:45 +0200

    first commit

---
+++ src/main/java/com/alx/springtravisci/SpringtravisciApplication.java
@@ -0,0 +1,13 @@
+package com.alx.springtravisci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtravisciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtravisciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alx/springtravisci/SpringtravisciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alx.springtravisci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtravisciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

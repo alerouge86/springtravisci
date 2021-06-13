97a2a98cd05d6dade019ca424a0502b6909a56a3
Alessandro Rossi <arossi@siapcn.it>
2021-06-13 20:28:43 +0200

    test: trying fixing .travis.yml

--- src/main/java/com/alx/springtravisci/controller/DemoController.java
+++ src/main/java/com/alx/springtravisci/controller/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello there! (removed test failing)";
+        return "hello there! 2.1";
     }
 }

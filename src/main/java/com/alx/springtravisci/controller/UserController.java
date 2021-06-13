2692678876aa6e723c4278be985f5d9102b6af7b
Alessandro Rossi <arossi@siapcn.it>
2021-06-13 13:38:25 +0200

    added /env in .gitignore

---
+++ src/main/java/com/alx/springtravisci/controller/UserController.java
@@ -0,0 +1,2 @@
+package com.alx.springtravisci.controller;public class UserController {
+}
---
+++ src/main/java/com/alx/springtravisci/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.alx.springtravisci.repository;
+import com.alx.awsdemo.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}

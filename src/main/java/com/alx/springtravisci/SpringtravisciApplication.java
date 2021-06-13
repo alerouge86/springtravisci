216e537c14f9444dfd760300500e3e98e04cc9f6
Alessandro Rossi <arossi@siapcn.it>
2021-06-13 20:13:35 +0200

    test: test deploy heroku

--- src/main/java/com/alx/springtravisci/SpringtravisciApplication.java
+++ src/main/java/com/alx/springtravisci/SpringtravisciApplication.java
@@ -8,6 +8,7 @@ public class SpringtravisciApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtravisciApplication.class, args);
+		System.out.println("SpringtravisciApplication is running...");
 	}
 
 }

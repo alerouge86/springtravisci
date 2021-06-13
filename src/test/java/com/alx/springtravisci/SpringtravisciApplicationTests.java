b0bc3e42039dc1855ceca50a4510945527dd2180
Alessandro Rossi <arossi@siapcn.it>
2021-06-13 15:58:36 +0200

    fixbug: removed db container for deploying to heroku

--- src/test/java/com/alx/springtravisci/SpringtravisciApplicationTests.java
+++ src/test/java/com/alx/springtravisci/SpringtravisciApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringtravisciApplicationTests {
 
 	@Test
